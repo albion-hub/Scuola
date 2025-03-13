3d67bae51f801a94d733fb4c8e95943bc258c13e
SHABANI ALBION <sSHABANI@itis.pr.it>
2025-03-13 14:13:49 +0100

    completare regioni in turtle

---
+++ informatica/pizzaroti/main.py
@@ -0,0 +1,13 @@
+import turtle
+import pandas
+
+screen = turtle.Screen()
+screen.title("states")
+
+image = "./italy-map.gif"
+screen.addshape(image)
+turtle.shape(image)
+
+
+
+screen.exitonclick()
