0a9535bb3b2ceaf93f531ec5707a1a881a2dc738
albion <salbion.shabani@itis.pr.it>
2025-03-19 14:33:58 +0100

    compito università finito :)

--- informatica/python/Gestione di un sistema universitario/main.py
+++ informatica/python/Gestione di un sistema universitario/main.py
@@ -1,11 +1,25 @@
 from persona import Persona
 from studente import Studente
+from professore import Professore
+from personaleAmministrativo import PersonaleAmministrativo
 from universita import Universita
 
-p = Studente("albion", "shabani", "17", "x")
+
+def stampa_descrizione(persona):
+    print(persona.descrizione())
+
+p = Persona("albion", "shabani", 17)
+s = Studente("albion", "shabani", 17, "x")
+prof = Professore("albion", "shabani", 21, "informatica", 2000)
+pA = PersonaleAmministrativo("albion","shabani", 21, "non lo so", 2100)
+
+p = [p,s,prof,pA]
+
+list(map(stampa_descrizione, p))
+
 
 u = Universita()
 u.aggiungi_persona(p)
 u.aggiungi_persona(p)
 #print(u.mostra_tutte_le_persone())
-print(u.trova_persone_per_tipo(Studente))
+#print(u.trova_persone_per_tipo(Studente))
--- informatica/python/Gestione di un sistema universitario/studente.py
+++ informatica/python/Gestione di un sistema universitario/studente.py
@@ -1,13 +1,17 @@
 from persona import Persona
-
+from professore import Professore
 class Studente(Persona):
-    def __init__(self, nome, cognome, eta, matricola):
+    def __init__(self, nome: str, cognome: str, eta: int, matricola: str,professore_referente: Professore):
         super().__init__(nome, cognome, eta)
         self.matricola = matricola
+        self.professore_referente = professore_referente
 
     def descrizione(self):
         return f"Sono uno studente {self.nome} {self.cognome}, matricola: {self.matricola}"
     
     def stampa_info(self):
-        return f"{super().descrizione()}, matricola: {self.matricola}"
+        return f"{super().descrizione()}, matricola: {self.matricola}, professore referente: {self.professore_referente}"
+    
+    def cambia_referente(self,nuovo_professore):
+        self.professore_referente = nuovo_professore
     
